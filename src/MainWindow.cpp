79c57dbc5f477bb5c8718f1a6dd6c1a0f127c64c
mrokita <mrokita@mrokita.pl>
2019-03-07 00:34:58 +0100

    Fix columns

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -28,7 +28,7 @@ MainWindow::MainWindow(json api_data) {
     m_TreeView.append_column("Country", m_Columns.m_col_country);
     m_TreeView.append_column("Population (total)", m_Columns.m_col_population_total);
     m_TreeView.append_column("Population (male)", m_Columns.m_col_population_male);
-    m_TreeView.append_column("Population (female)", m_Columns.m_col_population_male);
+    m_TreeView.append_column("Population (female)", m_Columns.m_col_population_female);
     m_ScrolledWindow.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_ALWAYS);
     m_ScrolledWindow.add(m_TreeView);
     add(m_ScrolledWindow);
