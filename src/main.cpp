a96024dcd12979fa4b3d39f0e35e09661ea8ecb9
mrokita <mrokita@mrokita.pl>
2019-03-06 23:27:02 +0100

    API connection

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,29 @@
 #include <curl/curl.h>
 #include "../include/MainWindow.h"
 
-int main(int argc, char *argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "pl.mrokita.proi1");
-
-    MainWindow window;
-
-    return app->run(window);
-}
-
-
-size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp)
+size_t curl_write(char *contents, size_t size, size_t nmemb, void *userp)
 {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
+
 void get_api_data(){
     curl_global_init(CURL_GLOBAL_ALL);
 
     CURL* handle = curl_easy_init();
     std::string buffer;
     curl_easy_setopt(handle, CURLOPT_URL, "http://api.population.io/1.0/population/2019/aged/18/");
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, curl_write);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buffer);
+    curl_easy_perform(handle);
+    std::cout << buffer;
 }
 
+int main(int argc, char *argv[]) {
+    get_api_data();
+    auto app = Gtk::Application::create(argc, argv, "pl.mrokita.proi1");
 
+    MainWindow window;
+
+    return app->run(window);
+}
